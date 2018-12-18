6a15b83aed1f7721a4dc142a979f915c4d3e9dfb
jubin <jubin@jubin.net>
2018-12-18 10:36:16 -0500

    Add a check for elevated privilege

--- windows/list_IIS_process_pid_apppool.cmd
+++ windows/list_IIS_process_pid_apppool.cmd
@@ -1,2 +1,17 @@
-cd c:\windows\system32\inetsrv
-appcmd list wp
+echo off
+set output=Done!
+pushd
+cd c:\windows\system32\inetsrv\
+
+rem start  - Check if Admin access
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    appcmd.exe list wp
+) else (
+    set output=NO Administrative permissions
+)
+rem end  - Check if Admin access
+
+popd
+echo %output%
+pause
